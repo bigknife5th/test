868d155e8d5c14d524934ccf1f74e2357c1a5aba
shawn <you@example.com>
2022-02-19 20:14:32 +0800

    test1

--- bin/test.dart
+++ bin/test.dart
@@ -6,7 +6,7 @@ void main(List<String> arguments) {
 
 Future<void> _test() async {
   FTPConnect ftpConnect = FTPConnect('192.168.0.139',
-      port: 21, user: 'bigknife', pass: 'Ab1234567');
+      port: 21, user: 'bigknife', pass: 'Ab1234567@@@g');
   bool res = false;
   res = await ftpConnect.connect();
   print('connect:$res');
