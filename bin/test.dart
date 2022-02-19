d4c21585e204735bac6e93cae5777deccded427d
shawn <you@example.com>
2022-02-19 19:52:48 +0800

    master

---
+++ bin/test.dart
@@ -0,0 +1,22 @@
+import 'package:ftpconnect/ftpconnect.dart';
+
+void main(List<String> arguments) {
+  _test();
+}
+
+Future<void> _test() async {
+  FTPConnect ftpConnect = FTPConnect('192.168.0.139',
+      port: 21, user: 'shawn', pass: 'Fasmot311');
+  bool res = false;
+  res = await ftpConnect.connect();
+  print('connect:$res');
+  //String a = await ftpConnect.currentDirectory();
+  DIR_LIST_COMMAND cmd = DIR_LIST_COMMAND.LIST;
+  List<FTPEntry> dirContent =
+      await ftpConnect.listDirectoryContent(cmd: DIR_LIST_COMMAND.NLST);
+  await Future.forEach(dirContent, (FTPEntry entry) async {
+    print(entry.name);
+  });
+  res = await ftpConnect.disconnect();
+  print('disconnect:$res');
+}
